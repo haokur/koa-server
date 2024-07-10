77ca295b5f157038595d067111d7af89816dd615
AllIsWell <admin@haokur.com>
2024-07-10 11:28:32 +0800

    ✨ feat: 不涉及具体业务的最基础的模板

--- src/abc.ts
+++
@@ -1,7 +0,0 @@
-function count(a: number, b: string) {
-    return a + b;
-}
-
-count(1, '1');
-
-console.log(111, 'abc.ts::7行');
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import Koa from 'koa';
 const app = new Koa();
 
 app.use(async (ctx) => {
-    const test = {
-        a: 1,
-    };
-    const b = 1;
-    console.log(b, 'main.ts::10行');
-    console.log(test, 'main.ts::9行');
     ctx.body = 'Hello,Koa!';
 });
 
