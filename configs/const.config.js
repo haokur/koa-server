f9dc62e9fc3abff8591ac8cc495d895ab126ba7b
haokur <haokur@users.noreply.github.com>
2024-07-11 13:27:17 +0000

    deploy: cacfa50c46cb916a57edac32d079696a8360c6f9

--- configs/const.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UPLOAD_DIR = void 0;
-const path_1 = __importDefault(require("path"));
-exports.UPLOAD_DIR = path_1.default.resolve(process.cwd(), './_temp');
--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const file_controller_1 = require("../controllers/file.controller");
-const user_controller_1 = require("../controllers/user.controller");
-const route = {
-    ...user_controller_1.UserRoutes,
-    ...file_controller_1.FileRoutes,
-};
-exports.default = route;
