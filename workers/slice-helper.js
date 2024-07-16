99a1c5412c992df4fb49b52e518ae3c92481f2f0
haokur <haokur@users.noreply.github.com>
2024-07-16 07:51:17 +0000

    deploy: d4a89801b5d033ab4dcbece7276d28bcb1fc60d8

---
+++ workers/slice-helper.js
@@ -0,0 +1,32 @@
+const WorkerHandler = {
+    sliceFile: async (payload) => {
+        const { file, start, end } = payload;
+        return file.slice(start, end);
+    },
+    sliceFileMany(payload) {
+        const { file, chunks } = payload;
+        const chunkData = [];
+        chunks.forEach((item) => {
+            const { start, end } = item;
+            chunkData.push(file.slice(start, end));
+        });
+        return chunkData;
+    },
+    sliceFileByTasks(payload) {
+        const { file, allTasks } = payload;
+        for (let i = 0; i < allTasks.length; i++) {
+            const { start, end } = allTasks[i];
+            file.slice(start, end);
+        }
+    },
+};
+
+self.onmessage = async function (event) {
+    const { _id, payload } = event.data;
+    const { action } = payload;
+    let result = null;
+    if (WorkerHandler[action]) {
+        result = await WorkerHandler[action](payload);
+    }
+    self.postMessage({ _id, payload: result });
+};
